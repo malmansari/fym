bafe4644d07e78d1afc32e36a1b30aadda1275fb
ynahshal <ynahshal@localhost>
2016-06-12 03:27:17 +0200

    Intial Commit

--- src/main/java/de/malmansari/playground/controller/HomeController.java
+++ src/main/java/de/malmansari/playground/controller/HomeController.java
@@ -3,11 +3,15 @@ package de.malmansari.playground.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HomeController {
 
 	@RequestMapping("/")
 	public String home() {
+		log.debug("Initial commit from yahya");
 		return "Das boot, reporting for duty!";
 	}
 }
